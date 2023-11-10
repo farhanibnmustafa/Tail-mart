473f89c879514f4c24f1ed9ec74f5dbc10a96a25
Farhan Ibn Mustafa <133661189+farhanibnmustafa@users.noreply.github.com>
2023-11-11 00:54:34 +0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    fontFamily: {
+      body:['Bebas Neue']
+    },
+    plugins: [],
+  }
+  module.exports = {
+    theme: {
+      fontFamily: {
+        'sans': ['ui-sans-serif', 'system-ui',],
+        'serif': ['ui-serif', 'Georgia',],
+        'mono': ['ui-monospace', 'SFMono-Regular',],
+        'display': ['Oswald',],
+        'body': ['"Open Sans"',],
+      }
+    }
+  }
